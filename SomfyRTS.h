b390bb720ffaefdbb07a47c5cb13cc2e1f5a9805
etimou <etienne.mouragnon@gmail.com>
2020-04-26 12:24:45 +0200

    added README and license

--- SomfyRTS.h
+++ SomfyRTS.h
@@ -1,3 +1,36 @@
+// **********************************************************************************
+// Somfy RTS Arduino library compatible with generic AM 433.42Mhz transmitter or RFM69
+// **********************************************************************************
+// Copyright Etienne Mouragnon (2020)
+// Based on previous work:
+// https://github.com/Nickduino/Somfy_Remote
+// https://github.com/kobuki/RFM69OOK 
+//
+// **********************************************************************************
+// License
+// **********************************************************************************
+// This program is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software
+// Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General
+// Public License along with this program.
+// If not, see <http://www.gnu.org/licenses/>.
+//
+// Licence can be viewed at
+// http://www.gnu.org/licenses/gpl-3.0.txt
+//
+// Please maintain this license information along with authorship
+// and copyright notices in any redistribution of this code
+// **********************************************************************************
 #ifndef SOMFY_RTS_H
 #define SOMFY_RTS_H
 #include <Arduino.h>
